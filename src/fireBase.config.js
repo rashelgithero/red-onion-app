54dc27f68923af5658f1a11b750776d8c14f43f4
unknown <rashelrana4006@gmail.com>
2023-06-15 20:30:10 +0600

    new adding

---
+++ src/fireBase.config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB1jHdy4gSx6DQ425SaahJ5EnJ_Y-mfoac",
+    authDomain: "red-onion-app-bfd0f.firebaseapp.com",
+    projectId: "red-onion-app-bfd0f",
+    storageBucket: "red-onion-app-bfd0f.appspot.com",
+    messagingSenderId: "335114863540",
+    appId: "1:335114863540:web:e8de9229e7a879dd9b28e1"
+  };
